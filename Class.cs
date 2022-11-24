57e66b3007992b484b186ac43bcf8a618cb9baca
WalkmanNik <nikitch2012@gmail.com>
2022-11-25 02:56:38 +0700

    Дополнение №2

---
+++ Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    partial class HotClass
+    {
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
